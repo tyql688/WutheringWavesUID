7a238dfb6398efbef71bbb2e1957d76970346308
typl688 <yury.zero.21@gmail.com>
2024-12-07 19:42:49 +0800

    🎨`调整wiki命令`

--- WutheringWavesUID/wutheringwaves_wiki/__init__.py
+++ WutheringWavesUID/wutheringwaves_wiki/__init__.py
@@ -24,24 +24,25 @@ sv_waves_guide = SV('鸣潮攻略')
 sv_waves_wiki = SV('鸣潮wiki')
 
 
-@sv_waves_wiki.on_prefix((
-    f'{PREFIX}角色wiki',
-    f'{PREFIX}角色介绍',
-    f'{PREFIX}角色命座',
-    f'{PREFIX}角色天赋',
-    f'{PREFIX}角色共鸣链',
-    f'{PREFIX}角色技能',
-    f'{PREFIX}武器wiki',
-    f'{PREFIX}武器介绍',
-    f'{PREFIX}声骸wiki',
-    f'{PREFIX}声骸介绍',
-))
+@sv_waves_guide.on_regex(rf'^{PREFIX}[\u4e00-\u9fa5]+(?:共鸣链|命座|天赋|技能|图鉴|wiki|介绍)$', block=True)
 async def send_waves_wiki(bot: Bot, ev: Event):
-    if "角色" in ev.command:
-        char_name = ev.text.strip(' ')
+    match = re.search(
+        rf'{PREFIX}(?P<wiki_name>[\u4e00-\u9fa5]+)(?P<wiki_type>共鸣链|命座|天赋|技能|图鉴|wiki|介绍)',
+        ev.raw_text
+    )
+    if not match:
+        return
+    ev.regex_dict = match.groupdict()
+    wiki_name = ev.regex_dict.get("wiki_name")
+    wiki_type = ev.regex_dict.get("wiki_type")
+
+    at_sender = True if ev.group_id else False
+    if wiki_type in ('共鸣链', '命座', '天赋', '技能'):
+        char_name = wiki_name
         char_id = char_name_to_char_id(char_name)
         if not char_id:
-            return f'[鸣潮] 角色名{char_name}无法找到, 可能暂未适配, 请先检查输入是否正确！'
+            msg = f'[鸣潮] wiki【{char_name}】无法找到, 可能暂未适配, 请先检查输入是否正确！\n'
+            return await bot.send(msg, at_sender)
 
         name = alias_to_char_name(char_name)
         if name == '漂泊者':
@@ -49,21 +50,26 @@ async def send_waves_wiki(bot: Bot, ev: Event):
         elif name == '漂泊者·湮灭':
             name = '漂泊者-女-湮灭'
         await bot.logger.info(f'[鸣潮] 开始获取{name}wiki')
-        query_role_type = "天赋" if "技能" in ev.command or "天赋" in ev.command else "命座"
+        query_role_type = "天赋" if "技能" in wiki_type or "天赋" in wiki_type else "命座"
         img = await draw_wiki_detail("共鸣者", name, query_role_type)
+        if isinstance(img, str):
+            msg = f"[鸣潮] wiki【{wiki_name}】无法找到, 可能暂未适配, 请先检查输入是否正确！\n"
+            return await bot.send(msg, at_sender)
         await bot.send(img)
-    elif "武器" in ev.command:
-        weapon_name = ev.text.strip(' ')
-        weapon_name = alias_to_weapon_name(weapon_name)
-        if not weapon_name:
-            return f'[鸣潮] 武器名{weapon_name}无法找到, 可能暂未适配, 请先检查输入是否正确！'
+    else:
+
+        weapon_name = alias_to_weapon_name(wiki_name)
         await bot.logger.info(f'[鸣潮] 开始获取{weapon_name}wiki')
         img = await draw_wiki_detail("武器", weapon_name)
-        await bot.send(img)
-    elif "声骸" in ev.command:
-        echo_name = ev.text.strip(' ')
-        await bot.logger.info(f'[鸣潮] 开始获取{echo_name}wiki')
-        img = await draw_wiki_detail("声骸", echo_name)
+        if isinstance(img, str):
+            echo_name = wiki_name
+            await bot.logger.info(f'[鸣潮] 开始获取{echo_name}wiki')
+            img = await draw_wiki_detail("声骸", echo_name)
+
+        if isinstance(img, str):
+            msg = f"[鸣潮] wiki【{wiki_name}】无法找到, 可能暂未适配, 请先检查输入是否正确！\n"
+            return await bot.send(msg, at_sender)
+
         await bot.send(img)
 
 
