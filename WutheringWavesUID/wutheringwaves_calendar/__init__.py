b65750d9964935ae5f3a7dd69d1e1ee2c0b7fee8
typl688 <yury.zero.21@gmail.com>
2024-12-11 10:44:00 +0800

    🐛`去掉日历的uid验证先`

--- WutheringWavesUID/wutheringwaves_calendar/__init__.py
+++ WutheringWavesUID/wutheringwaves_calendar/__init__.py
@@ -2,9 +2,6 @@ from gsuid_core.bot import Bot
 from gsuid_core.models import Event
 from gsuid_core.sv import SV
 from .draw_calendar_card import draw_calendar_img
-from ..utils.database.models import WavesBind
-from ..utils.error_reply import WAVES_CODE_103
-from ..utils.hint import error_reply
 from ..wutheringwaves_config import PREFIX
 
 sv_waves_calendar = SV('waves日历')
@@ -14,11 +11,11 @@ sv_waves_calendar = SV('waves日历')
     (f'{PREFIX}个人日历', f'{PREFIX}日历'), block=True
 )
 async def send_waves_calendar_pic(bot: Bot, ev: Event):
-    at_sender = True if ev.group_id else False
-    user_id = ev.at if ev.at else ev.user_id
-    uid = await WavesBind.get_uid_by_game(user_id, ev.bot_id)
-    if not uid:
-        return await bot.send(error_reply(WAVES_CODE_103), at_sender)
-
+    # at_sender = True if ev.group_id else False
+    # user_id = ev.at if ev.at else ev.user_id
+    # uid = await WavesBind.get_uid_by_game(user_id, ev.bot_id)
+    # if not uid:
+    #     return await bot.send(error_reply(WAVES_CODE_103), at_sender)
+    uid = ""
     im = await draw_calendar_img(ev, uid)
     return await bot.send(im)
