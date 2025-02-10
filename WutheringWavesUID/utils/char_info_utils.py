879277fa412f3c85a5c4e92a7799ea58ed4ea632
typl688 <yury.zero.21@gmail.com>
2025-02-10 23:24:11 +0800

    🎨`兼容面板的错误数据`

--- WutheringWavesUID/utils/char_info_utils.py
+++ WutheringWavesUID/utils/char_info_utils.py
@@ -8,15 +8,18 @@ from .resource.RESOURCE_PATH import PLAYER_PATH
 from ..utils.api.model import RoleDetailData
 
 
-async def get_all_role_detail_info_list(uid: str) -> Union[Generator[RoleDetailData, Any, None], None]:
+async def get_all_role_detail_info_list(
+    uid: str,
+) -> Union[Generator[RoleDetailData, Any, None], None]:
     path = PLAYER_PATH / uid / "rawData.json"
     if not path.exists():
         return None
     try:
-        async with aiofiles.open(path, mode='r', encoding='utf-8') as f:
+        async with aiofiles.open(path, mode="r", encoding="utf-8") as f:
             player_data = json.loads(await f.read())
     except Exception as e:
         logger.exception(f"get role detail info failed {path}:", e)
+        path.unlink(missing_ok=True)
         return None
 
     return iter(RoleDetailData(**r) for r in player_data)
