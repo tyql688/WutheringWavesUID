69c3f94cb5d65279044e62e602d4a01bdfcd02a1
eli stewart <3483948113@qq.com>
2025-03-05 17:05:54 +0800

    🌙支持国际服用户查询练度统计

--- WutheringWavesUID/wutheringwaves_charlist/draw_char_list.py
+++ WutheringWavesUID/wutheringwaves_charlist/draw_char_list.py
@@ -48,14 +48,19 @@ TEXT_PATH = Path(__file__).parent / "texture2d"
 
 
 async def draw_char_list_img(uid: str, ev: Event, user_id: str) -> Union[str, bytes]:
-    ck = await waves_api.get_ck(uid, user_id)
-    if not ck:
-        return error_reply(WAVES_CODE_102)
-    # 账户数据
-    succ, account_info = await waves_api.get_base_info(uid, ck)
-    if not succ:
-        return account_info
-    account_info = AccountBaseInfo(**account_info)
+    if waves_api.is_net(uid):
+        # 填充用户信息,name固定以免误会。creatTime=1 是为了满足.is_full的逻辑
+        account_info= AccountBaseInfo(name="国际服用户", id=uid, creatTime=1, level=0, worldLevel=0)
+    else:
+        ck = await waves_api.get_ck(uid, user_id)
+        if not ck:
+            return error_reply(WAVES_CODE_102)
+        # 账户数据
+        succ, account_info = await waves_api.get_base_info(uid, ck)
+
+        if not succ:
+            return account_info
+        account_info = AccountBaseInfo(**account_info)
 
     # 根据面板数据获取详细信息
     all_role_detail = None
