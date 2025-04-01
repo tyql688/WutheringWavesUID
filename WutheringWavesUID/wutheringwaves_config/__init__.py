db9d1890d57568f1768f13fde12ac959340fec8f
typl688 <yury.zero.21@gmail.com>
2025-04-01 23:07:12 +0800

    🎨本体暂时去掉开启关闭功能

--- WutheringWavesUID/wutheringwaves_config/__init__.py
+++ WutheringWavesUID/wutheringwaves_config/__init__.py
@@ -1,42 +1,18 @@
 import re
 
 from gsuid_core.bot import Bot
-from gsuid_core.logger import logger
 from gsuid_core.models import Event
 from gsuid_core.sv import SV, get_plugin_available_prefix
 
 from ..utils.database.models import WavesBind
 from ..utils.name_convert import alias_to_char_name
-from .set_config import set_config_func, set_waves_user_value
+from .set_config import set_waves_user_value
 from .wutheringwaves_config import WutheringWavesConfig
 
 sv_self_config = SV("鸣潮配置")
 
 
 PREFIX = get_plugin_available_prefix("WutheringWavesUID")
-
-
-@sv_self_config.on_prefix(("开启", "关闭"))
-async def open_switch_func(bot: Bot, ev: Event):
-    at_sender = True if ev.group_id else False
-    uid = await WavesBind.get_uid_by_game(ev.user_id, ev.bot_id)
-    if uid is None:
-        return await bot.send(
-            f"您还未绑定鸣潮特征码, 请使用【{PREFIX}绑定uid】完成绑定！", at_sender
-        )
-
-    from ..utils.waves_api import waves_api
-
-    ck = await waves_api.get_self_waves_ck(uid, ev.user_id)
-    if not ck:
-        from ..utils.error_reply import ERROR_CODE, WAVES_CODE_102
-
-        return await bot.send(f"当前特征码：{uid}\n{ERROR_CODE[WAVES_CODE_102]}")
-
-    logger.info(f"[{ev.user_id}]尝试[{ev.command[2:]}]了[{ev.text}]功能")
-
-    im = await set_config_func(ev, uid)
-    await bot.send(im, at_sender)
 
 
 @sv_self_config.on_prefix("设置")
--- WutheringWavesUID/wutheringwaves_config/set_config.py
+++ WutheringWavesUID/wutheringwaves_config/set_config.py
@@ -22,26 +22,3 @@ async def set_waves_user_value(ev: Event, func: str, uid: str, value: str):
         return f"设置成功!\n特征码[{uid}]\n当前{func}:{value}\n"
     else:
         return "设置失败!\n请检查参数是否正确!\n"
-
-
-async def set_config_func(ev: Event, uid: str = "0"):
-    config_name = ev.text
-    if "开启" in ev.command:
-        option = ev.group_id if ev.group_id else "on"
-    else:
-        option = "off"
-
-    logger.info(f"uid: {uid}, option: {option}, config_name: {config_name}")
-
-    other_msg = ""
-
-    return "该配置项不存在!"
-
-    # if option == "on":
-    #     succeed_msg = "开启至私聊消息!"
-    # elif option == "off":
-    #     succeed_msg = "关闭!"
-    # else:
-    #     succeed_msg = f"开启至群{option}"
-
-    # return f"{config_name}已{succeed_msg}\n{other_msg}"
