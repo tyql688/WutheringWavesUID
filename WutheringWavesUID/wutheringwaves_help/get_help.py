04ace5e50896c3157fb247e98e80090be99bef1d
typl688 <yury.zero.21@gmail.com>
2025-04-21 14:33:16 +0800

    🎨角色出场率v2

--- WutheringWavesUID/wutheringwaves_help/get_help.py
+++ WutheringWavesUID/wutheringwaves_help/get_help.py
@@ -1,14 +1,14 @@
 import json
-from pathlib import Path
 from typing import Dict
+from pathlib import Path
 
 from PIL import Image
-
-from gsuid_core.help.draw_new_plugin_help import get_new_help
 from gsuid_core.help.model import PluginHelp
+from gsuid_core.help.draw_new_plugin_help import get_new_help
+
 from ..utils.image import get_footer
-from ..version import WutheringWavesUID_version
 from ..wutheringwaves_config import PREFIX
+from ..version import WutheringWavesUID_version
 
 ICON = Path(__file__).parent.parent.parent / "ICON.png"
 HELP_DATA = Path(__file__).parent / "help.json"
@@ -41,6 +41,6 @@ async def get_help(pm: int):
         icon_path=ICON_PATH,
         footer=get_footer(),
         enable_cache=False,
-        column=4,
+        column=5,
         pm=pm,
     )
