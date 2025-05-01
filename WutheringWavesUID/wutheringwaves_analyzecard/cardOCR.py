379a4589b1040cbf6c3b72d9664068b5f6770aa4
eli stewart <3483948113@qq.com>
2025-05-01 21:49:49 +0800

    🌙识别结果优化-共鸣呜效率

--- WutheringWavesUID/wutheringwaves_analyzecard/cardOCR.py
+++ WutheringWavesUID/wutheringwaves_analyzecard/cardOCR.py
@@ -549,6 +549,7 @@ async def ocr_results_to_dict(chain_num, ocr_results):
             if re.search(r'暴.(傷害)?', attr):
                 attr = re.sub(r'暴.(傷害)?', r'暴擊\1', attr)
             attr = attr.replace("箓擎傷害", "暴擊傷害").replace("箓擎", "暴擊")
+            attr = re.sub(r'.*效率$', '共鳴效率', attr)
             clean_attr = cc.convert(attr) # 标准繁简转换
             # 验证属性名是否符合预期（至少两个中文字符，且不含数字）
             if len(clean_attr) >= 2 and not re.search(r'[0-9]', clean_attr):
