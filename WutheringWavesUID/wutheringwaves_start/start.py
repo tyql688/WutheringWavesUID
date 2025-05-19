bbb3ad143da73e7f8554ec68266a4ec0087b1dd9
typl688 <yury.zero.21@gmail.com>
2025-05-19 10:54:53 +0800

    🚑️请使用`core全部更新`&&`core重启`

--- WutheringWavesUID/wutheringwaves_start/start.py
+++ WutheringWavesUID/wutheringwaves_start/start.py
@@ -1,18 +1,31 @@
-import asyncio
-import threading
-
 from gsuid_core.logger import logger
+from gsuid_core.server import on_core_start
+
 from ..wutheringwaves_resource import startup
 
 
+@on_core_start
 async def all_start():
+    logger.info("[鸣潮] 启动中...")
     try:
+        from ..utils.damage.register_char import register_char
+        from ..utils.damage.register_echo import register_echo
+        from ..utils.damage.register_weapon import register_weapon
+        from ..utils.map.damage.register import register_damage, register_rank
+        from ..utils.queues import init_queues
+
+        # 注册
+        register_weapon()
+        register_echo()
+        register_damage()
+        register_rank()
+        register_char()
+
+        # 初始化任务队列
+        init_queues()
+
         await startup()
     except Exception as e:
         logger.exception(e)
 
-
-threading.Thread(
-    target=lambda: asyncio.run(all_start()),
-    daemon=True,
-).start()
+    logger.success("[鸣潮] 启动完成✅")
