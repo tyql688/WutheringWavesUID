28ef580d0f30db382740452afdcfb1d58c692365
typl688 <yury.zero.21@gmail.com>
2025-06-07 21:09:04 +0800

    🎨尝试修复

--- WutheringWavesUID/utils/api/requests.py
+++ WutheringWavesUID/utils/api/requests.py
@@ -79,26 +79,29 @@ async def _check_response(
     roleId=None,
 ) -> tuple[bool, Union[Dict, str]]:
     if isinstance(res, dict):
-        if res.get("code") == 200 and res.get("data"):
-            return True, res["data"]
+        res_code = res.get("code")
+        res_msg = res.get("msg")
+        res_data = res.get("data")
+        if res_code == 200 and res_data:
+            return True, res_data
 
-        if res.get("msg") and res.get("msg") == "请求成功":
+        if res_msg and (res_msg == "请求成功" or res_msg == "系统繁忙，请稍后再试"):
             msg = f"\n鸣潮账号id: 【{roleId}】未绑定库街区!!!\n1.是否注册过库街区\n2.库街区能否查询当前鸣潮账号数据\n"
             return False, error_reply(None, msg)
 
-        logger.warning(f"msg: {res.get('msg')} - data: {res.get('data')}")
+        logger.warning(f"[wwuid] code: {res_code} msg: {res_msg} data: {res_data}")
 
-        if res.get("msg") and ("重新登录" in res["msg"] or "登录已过期" in res["msg"]):
+        if res_msg and ("重新登录" in res_msg or "登录已过期" in res_msg):
             if token:
                 await WavesUser.mark_invalid(token, "无效")
-            return False, res.get("msg", "登录已过期")
+            return False, res_msg
 
-        if res.get("msg") and "访问被阻断" in res["msg"]:
-            await send_master_info(res.get("msg", "未知错误"))
+        if res_msg and "访问被阻断" in res_msg:
+            await send_master_info(res_msg)
             return False, error_reply(WAVES_CODE_998)
 
-        if res.get("msg"):
-            await send_master_info(res.get("msg", "未知错误"))
+        if res_msg:
+            await send_master_info(res_msg)
             return False, error_reply(WAVES_CODE_999)
     return False, error_reply(WAVES_CODE_999)
 
@@ -137,7 +140,6 @@ async def get_headers_ios():
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 18_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko)  KuroGameBox/2.5.0",
         "devCode": f"{ip}, Mozilla/5.0 (iPhone; CPU iPhone OS 18_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko)  KuroGameBox/2.5.0",
         "X-Forwarded-For": generate_random_ipv6_manual(),
-        "Access-Control-Request-Header": "b-at,devcode,did,source,token",
     }
     return header
 
